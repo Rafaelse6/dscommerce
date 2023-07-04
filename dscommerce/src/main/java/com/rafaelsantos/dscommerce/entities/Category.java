a334b6c506b3f7faffd5b9a86f16f547b33e9e83
Rafael Santos <rafa.05125@gmail.com>
2023-07-04 16:25:34 -0300

    Product, ProductDTO

--- dscommerce/src/main/java/com/rafaelsantos/dscommerce/entities/Category.java
+++ dscommerce/src/main/java/com/rafaelsantos/dscommerce/entities/Category.java
@@ -1,12 +1,15 @@
 package com.rafaelsantos.dscommerce.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Category implements Serializable{
 	
 	private String name;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
 	public Category() {}
 
 	public Category(Long id, String name) {
@@ -43,6 +49,10 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
