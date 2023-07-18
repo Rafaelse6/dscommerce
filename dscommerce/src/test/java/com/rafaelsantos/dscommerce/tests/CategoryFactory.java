e87928ddfc9b2d2303b2e07310eda842a9dcdf7a
Rafael Santos <rafa.05125@gmail.com>
2023-07-18 13:30:04 -0300

    CategoryFactory

---
+++ dscommerce/src/test/java/com/rafaelsantos/dscommerce/tests/CategoryFactory.java
@@ -0,0 +1,14 @@
+package com.rafaelsantos.dscommerce.tests;
+
+import com.rafaelsantos.dscommerce.entities.Category;
+
+public class CategoryFactory {
+	
+	public static Category createCategory() {
+		return new Category(1L, "Games");
+	}
+	
+	public static Category createCategory(Long id, String name) {
+		return new Category(id, name);
+	}
+}
