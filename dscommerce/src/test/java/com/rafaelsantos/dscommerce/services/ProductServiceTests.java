d878b82d929e827700d4a6140a7a5a6614402b14
Rafael Santos <rafa.05125@gmail.com>
2023-07-20 20:24:00 -0300

    Update Product tests

--- dscommerce/src/test/java/com/rafaelsantos/dscommerce/services/ProductServiceTests.java
+++ dscommerce/src/test/java/com/rafaelsantos/dscommerce/services/ProductServiceTests.java
@@ -25,6 +25,8 @@ import com.rafaelsantos.dscommerce.repositories.ProductRepository;
 import com.rafaelsantos.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.rafaelsantos.dscommerce.tests.ProductFactory;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
 
@@ -39,59 +41,78 @@ public class ProductServiceTests {
 	private Product product;
 	private ProductDTO productDTO;
 	private PageImpl<Product> page;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		existingProductId = 1L;
 		nonExistingProductId = 2L;
-		
+
 		productName = "Playstation 5";
 
 		product = ProductFactory.createProduct(productName);
 		productDTO = new ProductDTO(product);
 		page = new PageImpl<>(List.of(product));
-		
+
 		Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
-		
-		Mockito.when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);
-		
+
+		Mockito.when(repository.searchByName(any(), (Pageable) any())).thenReturn(page);
+
 		Mockito.when(repository.save(any())).thenReturn(product);
+
+		Mockito.when(repository.getReferenceById(existingProductId)).thenReturn(product);
+		Mockito.when(repository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnProductDTOWhenIdExists() {
 		ProductDTO result = service.findById(existingProductId);
-		
+
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getId(), existingProductId);
 		Assertions.assertEquals(result.getName(), product.getName());
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnResourceNotFoundExceptionWhenIdDoesNotExist() {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.findById(nonExistingProductId);
 		});
 	}
-	
+
 	@Test
 	public void findAllShouldReturnPagedProductMinDTO() {
 		Pageable pageable = PageRequest.of(0, 12);
 		String name = "Playstation 5";
-		
+
 		Page<ProductMinDTO> result = service.findAll(name, pageable);
-		
+
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getSize(), 1);
 		Assertions.assertEquals(result.iterator().next().getName(), productName);
 	}
-	
+
 	@Test
 	public void insertShouldReturnProductDTO() {
 		ProductDTO result = service.insert(productDTO);
-		
+
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getId(), product.getId());
+	}
+
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() {
+		ProductDTO result = service.update(existingProductId, productDTO);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), existingProductId);
+		Assertions.assertEquals(result.getName(), productDTO.getName());
+	}
+
+	@Test
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingProductId, productDTO);
+		});
 	}
 }
