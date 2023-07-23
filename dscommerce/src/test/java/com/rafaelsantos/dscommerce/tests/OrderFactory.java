a3b664261dd6948f788996df20d41661e2805470
Rafael Santos <rafa.05125@gmail.com>
2023-07-23 01:29:41 -0300

    OrderFactory

---
+++ dscommerce/src/test/java/com/rafaelsantos/dscommerce/tests/OrderFactory.java
@@ -0,0 +1,23 @@
+package com.rafaelsantos.dscommerce.tests;
+
+import java.time.Instant;
+
+import com.rafaelsantos.dscommerce.entities.Order;
+import com.rafaelsantos.dscommerce.entities.OrderItem;
+import com.rafaelsantos.dscommerce.entities.Payment;
+import com.rafaelsantos.dscommerce.entities.Product;
+import com.rafaelsantos.dscommerce.entities.User;
+import com.rafaelsantos.dscommerce.entities.enums.OrderStatus;
+
+public class OrderFactory {
+	
+	public static Order createOrder(User client) {
+		Order order = new Order(1L, Instant.now(), OrderStatus.WAITING_PAYMENT, client, new Payment());
+		
+		Product product = ProductFactory.createProduct();
+		OrderItem orderItem = new OrderItem(order, product, 2, 10.0);
+		order.getItems().add(orderItem);
+		
+		return order;
+	}
+}
